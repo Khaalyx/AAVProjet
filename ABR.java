d41317caf3abd25b36428dd2cd13039e49660f8b
Pebble <75127358+Pebbleuh@users.noreply.github.com>
2021-10-17 15:42:05 +0200

    Update ABR.java

--- ABR.java
+++ ABR.java
@@ -8,7 +8,7 @@ public class ABR {
     private int profondeur;
 
     public ABR(ArrayList<Objet> obj, ArrayList<Objet> objAMettre, float poidsMax, int i) {
-        this.listeObjets = obj;
+        this.listeObjets = obj; // objets du sac au noeud courant
         this.profondeur = i;
         calculBorneSup(obj);
         calculBorneInf();
@@ -16,12 +16,12 @@ public class ABR {
         // pas atteint la fin de la liste d'objets à mettre
         if (profondeur < objAMettre.size()) {
             this.filsGauche = new ABR(listeObjets, objAMettre, poidsMax, profondeur + 1);
-            ArrayList<Objet> listObj = new ArrayList<>();
-            if(obj != null) // si la liste d'objets n'est pas vide
+            ArrayList<Objet> listObj = new ArrayList<>(); // objets du sac + ajout d'un objet
+            if(obj != null) // si la liste d'objets du noeud courant n'est pas vide
                 for(Objet o : obj)
                     listObj.add(o); // on les copie dans la new list
-            listObj.add(objAMettre.get(profondeur)); // on y ajoute les objets à mettre au noeud courant
-            // poids de listObj (objets + objets à mettre?) < poidsMax
+            listObj.add(objAMettre.get(profondeur)); // on y ajoute l'objet à mettre au noeud courant
+            // poids de listObj (objets + objets en plus) <= poidsMax
             if (poidsListeObjets(listObj) <= poidsMax && borneSup > borneInf)
                 this.filsDroit = new ABR(listObj, objAMettre, poidsMax, profondeur + 1);
         }
