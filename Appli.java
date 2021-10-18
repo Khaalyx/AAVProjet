9e256dde3207a0e7cc667fc274a9fd58c91dac35
Lorie <83126685+Khaalyx@users.noreply.github.com>
2021-10-18 21:00:28 +0200

    Update Appli.java

--- Appli.java
+++ Appli.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Appli {
-
+    
     public static void main(String[] args) throws FileNotFoundException {
         Scanner sc = new Scanner(System.in);
         String s = "";
@@ -13,22 +13,22 @@ public class Appli {
             System.out.print("$>resoudre-sac-a-dos ");
             s = sc.nextLine();
 
+            // tant que la chaine de caract saisie n'est pas valide
             while (s.equals("") || !estValide(s)) {
                 System.out.print("$>sresoudre-sac-a-dos ");
                 s = sc.nextLine();
             }
 
-            Scanner scs = new Scanner(s);
-            String chemin = scs.next();
+            // stock les saisies dans un tableau
+            String[] str = s.split(" ");
+            String chemin = str[0];
             if(!chemin.equals("exit")) {
+                // la liste des objets qu'on doit mettre dans le sac
                 ArrayList<Objet> obj = lire(chemin);
-                SacADos sac = new SacADos(chemin, Float.parseFloat(scs.next()));
-                String methode = scs.next();
-                if (scs.hasNext())
-                    methode = methode + " " + scs.next();
+                SacADos sac = new SacADos(chemin, Float.parseFloat(str[1]));
+                String methode = str[2];
                 sac.resoudre(methode, obj);
-                scs.close();
-                System.out.println(sac);
+                System.out.println(sac); // affichage de la solution
             }
         }
         sc.close();
@@ -39,53 +39,34 @@ public class Appli {
      * @param s La chaîne de caractères saisie
      * @return true si la saisie est valide
      */
-
     private static boolean estValide(String s) {
-        Scanner scs = new Scanner(s);
-        String mot = scs.next();
+        String[] str = s.split(" ");
 
-        if(mot.equals("exit")) {
-            scs.close();
+        // si c'est la cmd pour arreter le prog
+        if(str.length == 1 && str[0].equals("exit"))
             return true;
-        }
 
-        File f = new File(mot);
-        if(!f.exists()) {
-            scs.close();
-            System.out.println("Fichier inexistant\n");
-            return false;
-        }
-
-        if(scs.hasNext()) {
-            mot = scs.next();
-            if (!mot.matches("-?\\d+(\\.\\d+)?")) {
-                scs.close();
+        if(str.length == 3) { // s'il y a le bon nomrbe d'arg
+            File f = new File(str[0]);
+            if (!f.exists()) { // vérifaction de l'existence du fichier
+                System.out.println("Fichier inexistant\n");
+                return false;
+            }
+            // s'il s'agit bien d'un nombre décimal
+            if (!str[1].matches("-?\\d+(\\.\\d+)?")) {
                 System.out.println("Le poids entré n'est pas valide\n");
                 return false;
             }
-            if (scs.hasNext()) {
-                mot = scs.next();
-                if (!(mot.equals("gloutonne") || mot.equals("pse"))) {
-                    if (mot.equals("prog.")) {
-                        mot = scs.next();
-                        if (!mot.equals("dynamique")) {
-                            System.out.println("Methode inexistante\n");
-                            scs.close();
-                            return false;
-                        }
-                    } else {
-                        System.out.println("Methode inexistante\n");
-                        scs.close();
-                        return false;
-                    }
-                }
-
-                scs.close();
-                return true;
+            // vérification de la méthode demandée
+            if (!(str[2].equals("gloutonne") || str[2].equals("pse")
+                    || str[2].equals("dynamique"))) {
+                System.out.println("Methode inexistante\n");
+                return false;
             }
+            return true;
         }
-        scs.close();
-        System.out.println("Paramètre manquant\n");
+        // s'il y a trop ou pas assez d'arg
+        System.out.println("Erreur dans le nombre de paramètre\n");
         return false;
     }
 
@@ -99,6 +80,7 @@ public class Appli {
         ArrayList<Objet> obj = new ArrayList<>();
         Scanner scan = new Scanner(new FileInputStream(file));
         while(scan.hasNextLine()){
+            // on stock dans un tableau les données d'une ligne du fichier
             String[] objet = scan.nextLine().split(";");
             if(objet != null) {
                 String nom = objet[0];
